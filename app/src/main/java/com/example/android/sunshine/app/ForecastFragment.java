6cfac7abfaa9e2d64fc0c835661dbdb4cc7e9a87
Dmytro Karataiev <karataev@MacBook-Pro-Dmytro.local>
2015-12-04 14:31:30 -0800

    add: settings for zip code works

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -1,9 +1,11 @@
 package com.example.android.sunshine.app;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.text.format.Time;
 import android.util.Log;
@@ -65,7 +67,12 @@ public class ForecastFragment extends Fragment {
         if (id == R.id.action_refresh) {
 
             FetchWeatherTask weatherTask = new FetchWeatherTask();
-            weatherTask.execute("90064");
+
+
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+            String zip = prefs.getString(getString(R.string.pref_location_key), getString(R.string.pref_location_default));
+            
+            weatherTask.execute(zip);
 
             return true;
         }
