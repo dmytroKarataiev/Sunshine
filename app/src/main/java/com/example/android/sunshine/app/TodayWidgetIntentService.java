c7a5035c48b42e3477d7edf3c809d35e0d708920
dmytroKarataiev <karataev.dima@gmail.com>
2016-01-14 21:11:05 -0800

    add: resizable widget

--- app/src/main/java/com/example/android/sunshine/app/TodayWidgetIntentService.java
+++ app/src/main/java/com/example/android/sunshine/app/TodayWidgetIntentService.java
@@ -9,6 +9,9 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.widget.RemoteViews;
 
 import com.example.android.sunshine.app.data.WeatherContract;
@@ -21,12 +24,14 @@ public class TodayWidgetIntentService extends IntentService {
     private static final String[] FORECAST_COLUMNS = {
             WeatherContract.WeatherEntry.COLUMN_WEATHER_ID,
             WeatherContract.WeatherEntry.COLUMN_SHORT_DESC,
-            WeatherContract.WeatherEntry.COLUMN_MAX_TEMP
+            WeatherContract.WeatherEntry.COLUMN_MAX_TEMP,
+            WeatherContract.WeatherEntry.COLUMN_MIN_TEMP
     };
 
     private static final int INDEX_WEATHER_ID = 0;
     private static final int INDEX_SHORT_DESC = 1;
     private static final int INDEX_MAX_TEMP = 2;
+    private static final int INDEX_MIN_TEMP = 3;
 
     public TodayWidgetIntentService() {
         super("TodayWidgetIntentService");
@@ -58,20 +63,39 @@ public class TodayWidgetIntentService extends IntentService {
 
         String description = data.getString(INDEX_SHORT_DESC);
         double maxTemp = data.getDouble(INDEX_MAX_TEMP);
+        double minTemp = data.getDouble(INDEX_MIN_TEMP);
 
         String formattedMaxTemp = Utility.formatTemperature(this, maxTemp, Utility.isMetric(this));
+        String formattedMinTemp = Utility.formatTemperature(this, minTemp, Utility.isMetric(this));
+
         data.close();
 
         for (int id : appWidgetIds) {
-            RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.weather_appwidget);
+            //Find the correct layout
+            int widgetWidth = getWidgetWidth(appWidgetManager, id);
+            int defaultWidth = getResources().getDimensionPixelSize(R.dimen.widget_today_default_width);
+            int largeWidth = getResources().getDimensionPixelSize(R.dimen.widget_today_large_width);
+            int layoutId;
+
+            if (widgetWidth >= largeWidth) {
+                layoutId = R.layout.weather_appwidget_big;
+            } else if (widgetWidth >= defaultWidth) {
+                layoutId = R.layout.weather_appwidget_medium;
+            } else {
+                layoutId = R.layout.weather_appwidget;
+            }
+
+            RemoteViews remoteViews = new RemoteViews(getPackageName(), layoutId);
 
             // Add the data to the RemoteViews
-            remoteViews.setImageViewResource(R.id.widget_image, weatherArtResourceId);
+            remoteViews.setImageViewResource(R.id.widget_icon, weatherArtResourceId);
             // Content Descriptions for RemoteViews were only added in ICS MR1
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1) {
                 setRemoteContentDescription(remoteViews, description);
             }
-            remoteViews.setTextViewText(R.id.widget_text, formattedMaxTemp);
+            remoteViews.setTextViewText(R.id.widget_description, description);
+            remoteViews.setTextViewText(R.id.widget_high_temperature, formattedMaxTemp);
+            remoteViews.setTextViewText(R.id.widget_low_temperature, formattedMinTemp);
 
             Intent launchIntent = new Intent(this, MainActivity.class);
             PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, launchIntent, 0);
@@ -85,6 +109,29 @@ public class TodayWidgetIntentService extends IntentService {
 
     @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1)
     private void setRemoteContentDescription(RemoteViews views, String description) {
-        views.setContentDescription(R.id.widget_image, description);
+        views.setContentDescription(R.id.widget_icon, description);
     }
+
+    private int getWidgetWidth(AppWidgetManager appWidgetManager, int appWidgetId) {
+        // Prior to Jelly Bean, widgets were always their default size
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+            return getResources().getDimensionPixelSize(R.dimen.widget_today_default_width);
+        }
+        // For Jelly Bean and higher devices, widgets can be resized - the current size can be
+        // retrieved from the newly added App Widget Options
+        return getWidgetWidthFromOptions(appWidgetManager, appWidgetId);
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    private int getWidgetWidthFromOptions(AppWidgetManager appWidgetManager, int appWidgetId) {
+        Bundle options = appWidgetManager.getAppWidgetOptions(appWidgetId);
+        if (options.containsKey(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH)) {
+            int minWidthDp = options.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH);
+            // The width returned is in dp, but we'll convert it to pixels to match the other widths
+            DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+            return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, minWidthDp, displayMetrics);
+        }
+        return  getResources().getDimensionPixelSize(R.dimen.widget_today_default_width);
+    }
+
 }
