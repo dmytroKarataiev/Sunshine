b2d1fe8941da26f19969ecc7015c50ff40a3c287
dmytroKarataiev <karataev.dima@gmail.com>
2016-01-14 19:01:46 -0800

    add: widget functions alpha

--- app/src/main/java/com/example/android/sunshine/app/TodayWidgetProvider.java
+++ app/src/main/java/com/example/android/sunshine/app/TodayWidgetProvider.java
@@ -1,10 +1,12 @@
 package com.example.android.sunshine.app;
 
+import android.annotation.TargetApi;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.widget.RemoteViews;
 
 /**
@@ -16,8 +18,21 @@ public class TodayWidgetProvider extends AppWidgetProvider {
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
 
+        int weatherArtResourceId = R.drawable.art_clear;
+        String description = "Clear";
+        double maxTemp = 24;
+        String formattedMaxTemperature = Utility.formatTemperature(context, maxTemp, Utility.isMetric(context));
+
         for (int id : appWidgetIds) {
             RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.weather_appwidget);
+
+            // Add the data to the RemoteViews
+            remoteViews.setImageViewResource(R.id.widget_image, weatherArtResourceId);
+            // Content Descriptions for RemoteViews were only added in ICS MR1
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1) {
+                setRemoteContentDescription(remoteViews, description);
+            }
+            remoteViews.setTextViewText(R.id.widget_text, formattedMaxTemperature);
 
             Intent intent = new Intent(context, MainActivity.class);
             PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
@@ -27,5 +42,10 @@ public class TodayWidgetProvider extends AppWidgetProvider {
             appWidgetManager.updateAppWidget(id, remoteViews);
         }
 
+    }
+
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1)
+    private void setRemoteContentDescription(RemoteViews views, String description) {
+        views.setContentDescription(R.id.widget_image, description);
     }
 }
